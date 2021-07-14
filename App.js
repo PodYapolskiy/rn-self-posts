2747be5075379183d11ff3b18c4a978acf345c70
Anatoly <podyapolskiyaa@gmail.com>
2021-07-14 12:15:42 +0300

    63. Создание приложения

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import AppLoading from 'expo-app-loading'
+import { StyleSheet, Text, View } from 'react-native'
+
+import { bootstrap } from './src/bootstrap'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [isReady, setIsReady] = useState(false)
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (isReady) {
+    return (
+      <AppLoading
+        startAsync={bootstrap}
+        onFinish={() => setIsReady(true)}
+        onError={err => console.log(err)}
+      />
+    )
+  }
+  return (
+    <View>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  )
+}
