e9520765b513ab3d2feb664de6105cad45210554
Anatoly <podyapolskiyaa@gmail.com>
2021-07-22 13:09:41 +0300

    93. Получение данных из базы

--- src/store/actions/post.js
+++ src/store/actions/post.js
@@ -1,9 +1,17 @@
+import { DB } from '../../db'
 import { ADD_POST, LOAD_POSTS, REMOVE_POST, TOGGLE_BOOKED } from '../types'
 
 export const loadPosts = () => {
-  return {
-    type: LOAD_POSTS,
-    payload: [], // Данные, которые передаются
+  // Возвращаем ассинхронный колбэк
+  return async dispatch => {
+    // Внутри колбэка делаем ассинхронный запрос в базу данных
+    const posts = await DB.getPosts()
+
+    // Диспатчим готовые данные в приложение
+    dispatch({
+      type: LOAD_POSTS,
+      payload: posts, // Данные, которые передаются
+    })
   }
 }
 
